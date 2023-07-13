ae3b0406c410127521828bfec31dda5b884ffc26
bkproworld <bkrv7799@gmail.com>
2023-07-13 20:19:09 +0530

    3rd commit

--- AdvJenkins/src/test/java/Practicemvn/LaunchTest.java
+++ AdvJenkins/src/test/java/Practicemvn/LaunchTest.java
@@ -1,11 +1,16 @@
 package Practicemvn;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class LaunchTest {
 
 	@Test
 	public void test1() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.flipkart.com");
 		System.out.println("Practice for jenkins");
 		System.out.println("new program added");
 	}
